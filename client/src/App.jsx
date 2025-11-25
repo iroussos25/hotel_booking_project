6d615f1c482d33ad3693370370206d59e77f15c1
Yiannis <ioannis.roussos@hotmail.com>
2025-11-25 18:55:55 +0200

    working on hero section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from './components/Navbar'
-import { useLocation } from 'react-router-dom'
+import { Route, Routes, useLocation } from 'react-router-dom'
+import Home from './pages/Home'
 
 const App = () => {
 
@@ -8,6 +9,11 @@ const isOwnerPath=useLocation().pathname.includes("owner")
   return (
     <div>
       {!isOwnerPath && <Navbar />}
+    <div classname='min-h-[70vh]'>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+      </Routes>
+    </div>
     </div>
   )
 }

2645d10f774e8f6dbdbdb7b51cfa7123663d9988
Yiannis <ioannis.roussos@hotmail.com>
2025-11-27 21:04:58 +0200

    adding ameneties, something wrong

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,13 +10,14 @@ const App = () => {
 
 const isOwnerPath=useLocation().pathname.includes("owner")
   return (
+    
     <div>
       {!isOwnerPath && <Navbar />}
     <div className='min-h-[70vh]'>
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/rooms' element={<AllRooms/>}/>
-        <Route path='/rooms/id' element={<RoomDetails/>}/>
+        <Route path='/rooms/:id' element={<RoomDetails/>}/>
       </Routes>
     </div>
      <Footer />
--- client/src/pages/AllRooms.jsx
+++ client/src/pages/AllRooms.jsx
@@ -90,7 +90,7 @@ const AllRooms = () => {
       {/*Filters*/}
       <div className='bg-white w-80 border border-gray-300 text-gray-600 max-lg:mb-8 lg:mt-16'>
 
-        <div className={`flex items-center justify-between px-5 py-2.5 min-lg:border-b border-gray-300 ${openFilters && "border-b"}`}>
+        <div className={`flex items-center justify-between px-5 py-2.5 lg:border-b border-gray-300 ${openFilters && "border-b"}`}>
             <p className='text-base font-medium text-gray-800'>FILTERS</p>
             <div className='text-xs cursor-pointer'>
                 <span onClick={()=> setOpenFilters(!openFilters)} className='lg:hidden'>{openFilters ? 'HIDE' : 'SHOW'}</span>

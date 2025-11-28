03f1e0692d1a1c0bf63d6832b970d5ae1d62406a
Yiannis <ioannis.roussos@hotmail.com>
2025-11-28 14:46:01 +0200

    working on owner pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,8 @@ import Footer from './components/Footer'
 import AllRooms from './pages/AllRooms'
 import RoomDetails from './pages/RoomDetails'
 import MyBookings from './pages/MyBookings'
+import HotelReg from './components/HotelReg'
+import Layout from './pages/HotelOwner/Layout'
 
 const App = () => {
 
@@ -14,12 +16,16 @@ const isOwnerPath=useLocation().pathname.includes("owner")
     
     <div>
       {!isOwnerPath && <Navbar />}
+      {false && <HotelReg />}
     <div className='min-h-[70vh]'>
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/rooms' element={<AllRooms/>}/>
         <Route path='/rooms/:id' element={<RoomDetails/>}/>
         <Route path='/my-bookings' element={<MyBookings/>}/>
+        <Route path='/owner' element={<Layout/>}>
+
+        </Route>
       </Routes>
     </div>
      <Footer />

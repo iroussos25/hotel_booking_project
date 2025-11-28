506570359af63553f5cf37e7ba5ee969ee622d1d
Yiannis <ioannis.roussos@hotmail.com>
2025-11-28 19:46:29 +0200

    finished add room page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,9 @@ import RoomDetails from './pages/RoomDetails'
 import MyBookings from './pages/MyBookings'
 import HotelReg from './components/HotelReg'
 import Layout from './pages/HotelOwner/Layout'
+import Dashboard from './pages/HotelOwner/Dashboard'
+import AddRoom from './components/HotelOwner/AddRoom'
+import ListRoom from './pages/HotelOwner/ListRoom'
 
 const App = () => {
 
@@ -24,7 +27,9 @@ const isOwnerPath=useLocation().pathname.includes("owner")
         <Route path='/rooms/:id' element={<RoomDetails/>}/>
         <Route path='/my-bookings' element={<MyBookings/>}/>
         <Route path='/owner' element={<Layout/>}>
-
+        <Route index element={<Dashboard/>}/>
+        <Route path='add-room' element={<AddRoom/>}/>
+        <Route path='list-room' element={<ListRoom/>}/>
         </Route>
       </Routes>
     </div>
